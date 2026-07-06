8ee91f8814306958e7f539545c6fe3d3d06e66e2
baluguriharshitha <baluguriharshitha@gmail.com>
2026-07-06 09:58:29 +0530

    Updated user schemas
    Updated email field to use EmailStr for validation.

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,17 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class UserRegister(BaseModel):
-
     full_name: str
-
-    email: str
-
+    email: EmailStr
     password: str
 
 
 class UserLogin(BaseModel):
-
-    email: str
-
+    email: EmailStr
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
