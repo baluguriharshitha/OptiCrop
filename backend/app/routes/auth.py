81511f90d5ae13b610e07816f6f7bb0a0c1fc03d
baluguriharshitha <baluguriharshitha@gmail.com>
2026-07-06 10:15:57 +0530

    Implemented registration and login APIs

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,4 +1,24 @@
 from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+
+from app.schemas.user import (
+    UserRegister,
+    UserLogin
+)
+
+from app.services.user_service import (
+    create_user,
+    authenticate_user
+)
+
+from app.auth.jwt_handler import (
+    create_access_token
+)
 
 router = APIRouter(
     prefix="/auth",
@@ -6,9 +26,55 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-def auth_home():
+@router.post("/register")
+def register(
+    request: UserRegister,
+    db: Session = Depends(get_db)
+):
+
+    user = create_user(
+        db,
+        request.full_name,
+        request.email,
+        request.password
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already exists"
+        )
 
     return {
-        "message": "Authentication Module Ready"
+        "message": "Registration Successful"
+    }
+
+
+@router.post("/login")
+def login(
+    request: UserLogin,
+    db: Session = Depends(get_db)
+):
+
+    user = authenticate_user(
+        db,
+        request.email,
+        request.password
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid Credentials"
+        )
+
+    token = create_access_token(
+        {
+            "sub": user.email
+        }
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
     }
