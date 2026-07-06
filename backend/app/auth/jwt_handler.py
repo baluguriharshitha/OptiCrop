199ece384721e23828d8f7bdc8d45622bbafcc27
baluguriharshitha <baluguriharshitha@gmail.com>
2026-07-06 10:11:59 +0530

    Updated JWT token handler

--- backend/app/auth/jwt_handler.py
+++ backend/app/auth/jwt_handler.py
@@ -1,26 +1,21 @@
-from datetime import datetime
-from datetime import timedelta
-
+from datetime import datetime, timedelta
 from jose import jwt
 
 from app.config import (
     SECRET_KEY,
     ALGORITHM,
-    ACCESS_TOKEN_EXPIRE_MINUTES,
+    ACCESS_TOKEN_EXPIRE_MINUTES
 )
 
 
 def create_access_token(data: dict):
-
     to_encode = data.copy()
 
     expire = datetime.utcnow() + timedelta(
         minutes=ACCESS_TOKEN_EXPIRE_MINUTES
     )
 
-    to_encode.update(
-        {"exp": expire}
-    )
+    to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(
         to_encode,
