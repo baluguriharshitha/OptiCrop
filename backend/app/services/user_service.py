7127ad6edf3838a406ea7a5b6f0f3407d22bbcc1
baluguriharshitha <baluguriharshitha@gmail.com>
2026-07-06 10:13:06 +0530

    Added user authentication service
    Refactor create_user function to include type hints and add authenticate_user function for user authentication.

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -2,10 +2,25 @@ from sqlalchemy.orm import Session
 
 from app.models.user import User
 
-from app.auth.password import hash_password
+from app.auth.password import (
+    hash_password,
+    verify_password
+)
 
 
-def create_user(db: Session, full_name, email, password):
+def create_user(
+    db: Session,
+    full_name: str,
+    email: str,
+    password: str
+):
+
+    existing = db.query(User).filter(
+        User.email == email
+    ).first()
+
+    if existing:
+        return None
 
     user = User(
         full_name=full_name,
@@ -18,5 +33,27 @@ def create_user(db: Session, full_name, email, password):
     db.commit()
 
     db.refresh(user)
+
+    return user
+
+
+def authenticate_user(
+    db: Session,
+    email: str,
+    password: str
+):
+
+    user = db.query(User).filter(
+        User.email == email
+    ).first()
+
+    if not user:
+        return None
+
+    if not verify_password(
+        password,
+        user.hashed_password
+    ):
+        return None
 
     return user
